fdb65635613df1829adbd53f130f208898b14308
Senfo <lpsenfo@gmail.com>
2021-05-23 17:54:48 +0200

    config parser

---
+++ src/util/config-parser.ts
@@ -0,0 +1,174 @@
+
+/**
+ * Config parser ported from arma-config-parser
+ */
+export class ConfigParser {
+
+    private debug: boolean = false;
+
+    private find(input: string, ptr: number, search: string): number {
+        const idx = input.indexOf(search, ptr);
+        if (idx !== -1) {
+            return (idx - ptr) + search.length;
+        }
+        return -1;
+    }
+
+    public cfg2json(input: string, level?: number): any {
+        level = (level || 0) + 1;
+
+        input = input
+            .replace(/\r\n/g, '\n')
+            .replace(/\n\n/g, '\n');
+
+        let ptr = 0;
+        const output = {};
+
+        let classMatch = null;
+        let arrMatch = null;
+        let stringMatch = null;
+        let boolMatch = null;
+        let numberMatch = null;
+
+        while (ptr < input.length) {
+            let eol = this.find(input, ptr, '\n');
+            if (eol === -1) break;
+            const line = input.slice(ptr, ptr + eol).trim();
+            // eslint-disable-next-line no-cond-assign
+            if (classMatch = line.match(/^class\s+([a-zA-Z0-9]+)/i)) {
+                if (this.debug) {
+                    console.log('classMatch', classMatch[1], level);
+                }
+                ptr += this.find(input, ptr, '{') + 1;
+                const innerStart = ptr;
+                let parenthesisCount = 1;
+                while (parenthesisCount > 0) {
+                    const c = input[ptr];
+                    if (!c) {
+                        throw new Error('unexpected end of file - prob a bug when parsing the cfg');
+                    }
+                    if (c === '"' || c === '\'') {
+                        do {
+                            ptr += 1;
+                            ptr += this.find(input, ptr, c);
+                        } while (input[ptr] === c);
+                        continue;
+                    } else if (c === '{') {
+                        parenthesisCount += 1;
+                    } else if (c === '}') {
+                        parenthesisCount -= 1;
+                    }
+                    ptr += 1;
+                }
+                output[classMatch[1]] = this.cfg2json(
+                    `${input.slice(innerStart, ptr - 1).trim()}\n`,
+                    level,
+                );
+                eol = this.find(input, ptr, '\n');
+            // eslint-disable-next-line no-cond-assign
+            } else if (stringMatch = line.match(/^([a-zA-Z0-9]+)\s*=\s*"(.*)";/i)) {
+                if (this.debug) {
+                    console.log('stringMatch', stringMatch[1], level);
+                }
+                // eslint-disable-next-line prefer-destructuring
+                output[stringMatch[1]] = stringMatch[2] ?? '';
+            // eslint-disable-next-line no-cond-assign
+            } else if (boolMatch = line.match(/^([a-zA-Z0-9]+)\s*=\s*(true|false);/i)) {
+                if (this.debug) {
+                    console.log('boolMatch', boolMatch[1], level);
+                }
+                // eslint-disable-next-line prefer-destructuring
+                output[boolMatch[1]] = boolMatch[2] === 'true';
+            // eslint-disable-next-line no-cond-assign
+            } else if (numberMatch = line.match(/^([a-zA-Z0-9]+)\s*=\s*([0-9\.\-e]+);/i)) {
+                if (this.debug) {
+                    console.log('numberMatch', numberMatch[1], level);
+                }
+                output[numberMatch[1]] = parseFloat(numberMatch[2]);
+            // eslint-disable-next-line no-cond-assign
+            } else if (arrMatch = line.match(/^([a-zA-Z0-9]+)\s*\[\]\s*=\s*/i)) {
+                if (this.debug) {
+                    console.log('arrMatch', arrMatch[1], level);
+                }
+                ptr = input.indexOf('{', ptr) + 1;
+                const innerStart = ptr;
+                let parenthesisCount = 1;
+                while (parenthesisCount > 0) {
+                    const c = input[ptr];
+                    if (c === '"' || c === '\'') {
+                        ptr = input.indexOf(c, ptr + 1);
+                    } else if (c === '{') {
+                        parenthesisCount += 1;
+                    } else if (c === '}') {
+                        parenthesisCount -= 1;
+                    }
+                    ptr += 1;
+                }
+                const inner = `[${input.slice(innerStart, ptr - 1).trim()}]`;
+                output[arrMatch[1]] = JSON.parse(inner);
+                eol = this.find(input, ptr, '\n');
+            }
+            ptr += eol;
+        }
+        return output;
+    }
+
+
+    public json2cfg(input: string, indent? : number): string {
+        indent = indent || 0;
+        const tabs = '\t'.repeat(indent);
+        const output = [];
+        Object.keys(input).forEach((key) => {
+            const val = input[key];
+            let type: string = typeof val;
+            if (type === 'object' && Array.isArray(val)) {
+                type = 'array';
+            }
+            switch (type) {
+                case 'number':
+                case 'boolean': {
+                    output.push(`${tabs}${key}=${val};`);
+                    break;
+                }
+                case 'string': {
+                    output.push(`${tabs}${key}="${val}";`);
+                    break;
+                }
+                case 'array': {
+                    if (val.some((v) => typeof v === 'string')) {
+                        output.push(
+                            `${tabs}${key}[]=`,
+                            `${tabs}{`,
+                        );
+                        val.forEach((v, i, a) => {
+                            if (isNaN(v)) {
+                                output.push(`${tabs}\t"${v}"${(i === (a.length - 1)) ? '' : ','}`);
+                            } else {
+                                output.push(`${tabs}\t${v}${(i === (a.length - 1)) ? '' : ','}`);
+                            }
+                        });
+                        output.push(`${tabs}};`);
+                    } else {
+                        let line = '';
+                        val.forEach((v, i) => {
+                            line = `${line}${v}${(i === (val.length - 1)) ? '' : ','}`;
+                        });
+                        output.push(`${tabs}${key}[]={${line}};`);
+                    }
+                    break;
+                }
+                case 'object': {
+                    output.push(
+                        `${tabs}class ${key}`,
+                        `${tabs}{`,
+                        this.json2cfg(val, indent + 1),
+                        `${tabs}};`,
+                    );
+                    break;
+                }
+            }
+        });
+        return (indent === 0) ? (`${output.join('\r\n')}\r\n`) : output.join('\r\n');
+    }
+
+}
---
+++ test/util/config-parser.test.ts
@@ -0,0 +1,112 @@
+import { expect } from '../expect';
+import { ImportMock } from 'ts-mock-imports'
+import * as path from 'path';
+import * as fs from 'fs';
+import * as fse from 'fs-extra';
+import * as child_process from 'child_process';
+import { ConfigParser } from '../../src/util/config-parser';
+import { disableConsole, enableConsole } from '../util';
+
+const DEFAULT_CFG = `
+hostname = "EXAMPLE NAME";  // Server name
+password = "";              // Password to connect to the server
+passwordAdmin = "";         // Password to become a server admin
+
+motd[] = { "line1","line2" }; // motd
+motd2[] = {};               // motd2
+
+enableWhitelist = 0;        // Enable/disable whitelist (value 0-1)
+ 
+maxPlayers = 60;            // Maximum amount of players
+ 
+verifySignatures = 2;       // Verifies .pbos against .bisign files. (only 2 is supported)
+forceSameBuild = 1;         // When enabled, the server will allow the connection only to clients with same the .exe revision as the server (value 0-1)
+ 
+disableVoN = 0;             // Enable/disable voice over network (value 0-1)
+vonCodecQuality = 20;        // Voice over network codec quality, the higher the better (values 0-30)
+ 
+disable3rdPerson = 0;         // Toggles the 3rd person view for players (value 0-1)
+disableCrosshair = 0;         // Toggles the cross-hair (value 0-1)
+
+disablePersonalLight = 1;   // Disables personal light for all clients connected to server
+lightingConfig = 0;         // 0 for brighter night setup, 1 for darker night setup
+ 
+serverTime = "SystemTime";    // Initial in-game time of the server. "SystemTime" means the local time of the machine. Another possibility is to set the time to some value in "YYYY/MM/DD/HH/MM" format, f.e. "2015/4/8/17/23" .
+serverTimeAcceleration = 12;  // Accelerated Time (value 0-24)// This is a time multiplier for in-game time. In this case, the time would move 24 times faster than normal, so an entire day would pass in one hour.
+serverNightTimeAcceleration = 1;  // Accelerated Nigh Time - The numerical value being a multiplier (0.1-64) and also multiplied by serverTimeAcceleration value. Thus, in case it is set to 4 and serverTimeAcceleration is set to 2, night time would move 8 times faster than normal. An entire night would pass in 3 hours.
+serverTimePersistent = 0;     // Persistent Time (value 0-1)// The actual server time is saved to storage, so when active, the next server start will use the saved time value.
+ 
+guaranteedUpdates = 1;        // Communication protocol used with game server (use only number 1)
+ 
+loginQueueConcurrentPlayers = 5;  // The number of players concurrently processed during the login process. Should prevent massive performance drop during connection when a lot of people are connecting at the same time.
+loginQueueMaxPlayers = 500;       // The maximum number of players that can wait in login queue
+ 
+instanceId = 1;             // DayZ server instance id, to identify the number of instances per box and their storage folders with persistence files
+
+storeHouseStateDisabled = false;// Disable houses/doors persistence (value true/false), usable in case of problems with persistence
+storageAutoFix = 1;         // Checks if the persistence files are corrupted and replaces corrupted ones with empty ones (value 0-1)
+
+ 
+class Missions
+{
+    class DayZ
+    {
+        template="dayzOffline.chernarusplus"; // Mission to load on server startup. <MissionName>.<TerrainName>
+					      // Vanilla mission: dayzOffline.chernarusplus
+					      // DLC mission: dayzOffline.enoch
+    };
+};
+`;
+
+describe('Test class config parser', () => {
+
+    before(() => {
+        disableConsole();
+    });
+
+    after(() => {
+        enableConsole();
+    });
+
+    beforeEach(() => {
+        // restore mocks
+        ImportMock.restore();
+    });
+
+    it('ConfigParser-cfg2json-json2cfg', () => {
+
+        const parser = new ConfigParser();
+        
+        const parsed = parser.cfg2json(DEFAULT_CFG);
+
+        expect(parsed).to.be.not.undefined;
+        expect(parsed.hostname).to.equal("EXAMPLE NAME");
+        expect(parsed.password).to.equal("");
+        expect(parsed.motd.length).to.equal(2);
+        expect(parsed.motd[0]).to.equal("line1");
+        expect(parsed.motd[1]).to.equal("line2");
+        expect(parsed.motd2.length).to.equal(0);
+        expect(parsed.disable3rdPerson).to.equal(0);
+        expect(parsed.serverTime).to.equal("SystemTime");
+        expect(parsed.storeHouseStateDisabled).to.equal(false);
+        expect(parsed.storageAutoFix).to.equal(1);
+ 
+        expect(parsed?.Missions?.DayZ?.template).to.equal("dayzOffline.chernarusplus");
+
+        const stringified = parser.json2cfg(parsed);
+        expect(stringified).to.be.not.undefined;
+        expect(stringified).to.be.not.null;
+        expect(stringified).to.contain('hostname="EXAMPLE NAME"');
+        expect(stringified).to.contain('password=""');
+        expect(stringified).to.contain('motd[]');
+        expect(stringified).to.contain('"line1"');
+        expect(stringified).to.contain('"line2"');
+        expect(stringified).to.contain('motd2[]={}');
+        expect(stringified).to.contain('disable3rdPerson=0');
+        expect(stringified).to.contain('serverTime="SystemTime"');
+        expect(stringified).to.contain('storeHouseStateDisabled=false');
+        expect(stringified).to.contain('storageAutoFix=1');
+        
+    });
+
+});
