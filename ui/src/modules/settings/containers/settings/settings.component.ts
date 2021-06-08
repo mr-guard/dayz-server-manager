85084a5463e9e35bc0bc892a420a158a55d2fa72
Senfo <lpsenfo@gmail.com>
2021-06-08 20:50:17 +0200

    server cfg in settings editor

--- ui/src/modules/settings/containers/settings/settings.component.ts
+++ ui/src/modules/settings/containers/settings/settings.component.ts
@@ -4,6 +4,16 @@ import { AppCommonService } from '@common/services';
 
 import configschema from '../../../../../../src/config/config.schema.json';
 
+type ServerCfgKey = keyof typeof configschema.definitions.ServerCfg.properties;
+
+interface Property {
+    name: string;
+    description: string;
+    enum?: (string | number)[];
+    type: 'number' | 'string' | 'boolean';
+    default: any;
+}
+
 @Component({
     selector: 'sb-settings',
     changeDetection: ChangeDetectionStrategy.Default,
@@ -21,6 +31,8 @@ export class SettingsComponent implements OnInit {
         message: string;
         success: boolean;
     };
+
+    public serverCfgProps = this.getServerCfgProps();
 
     public constructor(
         public appCommon: AppCommonService,
@@ -69,6 +81,19 @@ export class SettingsComponent implements OnInit {
             channel: '',
             mode: 'admin',
         });
+    }
+
+    private getServerCfgProps(): Property[] {
+        return (this.schema.definitions.ServerCfg.propertyOrder as ServerCfgKey[])
+            .filter((x) => {
+                const { type } = this.schema.definitions.ServerCfg.properties[x];
+                return ['string', 'number'].includes(type)
+                    && !(['motd', 'motdInterval', 'Missions'] as ServerCfgKey[]).includes(x);
+            })
+            .map((x) => ({
+                ...(this.schema.definitions.ServerCfg.properties[x] as Property),
+                name: x,
+            }));
     }
 
 }
