c7afb81f9591fdcd526b0316ab1217901a68cee5
mr-guard <lpsenfo@gmail.com>
2021-07-26 07:21:27 +0200

    log outcome of events

--- src/services/events.ts
+++ src/services/events.ts
@@ -17,54 +17,70 @@ export class Events implements IStatefulService {
     public async start(): Promise<void> {
         for (const event of (this.manager.config.events ?? [])) {
 
-            const checkAndRun = async (task: () => any): Promise<void> => {
-                if (this.manager?.monitor?.serverState !== ServerState.STARTED) {
-                    this.log.log(LogLevel.WARN, `Skipping ${event.name} because server is not in STARTED state`);
-                    return;
-                }
-                task();
+            const runTask = async (task: () => Promise<any>): Promise<any> => {
+                void task()
+                    .then(() => {
+                        this.log.log(LogLevel.DEBUG, `Successfully excuted task '${event.name}'`);
+                    })
+                    .catch(() => {
+                        this.log.log(LogLevel.WARN, `Failed to excute task '${event.name}'`);
+                    });
             };
 
-            this.tasks.push(
-                cron.scheduleJob(
-                    event.name,
-                    event.cron,
-                    () => {
-                        switch (event.type) {
-                            case 'restart': {
-                                void checkAndRun(() => {
-                                    void this.manager.discord.relayRconMessage('Scheduled Restart!');
-                                    void this.manager.monitor.killServer();
-                                });
-                                break;
-                            }
-                            case 'message': {
-                                void checkAndRun(() => void this.manager.rcon.global(event.params[0]));
-                                break;
-                            }
-                            case 'kickAll': {
-                                void checkAndRun(() => void this.manager.rcon.kickAll());
-                                break;
-                            }
-                            case 'lock': {
-                                void checkAndRun(() => void this.manager.rcon.lock());
-                                break;
-                            }
-                            case 'unlock': {
-                                void checkAndRun(() => void this.manager.rcon.unlock());
-                                break;
-                            }
-                            case 'backup': {
-                                void this.manager.backup.createBackup();
-                                break;
-                            }
-                            default: {
-                                break;
-                            }
+            const checkAndRun = async (task: () => Promise<any>): Promise<void> => {
+                if (this.manager?.monitor?.serverState !== ServerState.STARTED) {
+                    this.log.log(LogLevel.WARN, `Skipping '${event.name}' because server is not in STARTED state`);
+                    return;
+                }
+
+                void runTask(task);
+            };
+
+            const job = cron.scheduleJob(
+                event.name,
+                event.cron,
+                () => {
+                    switch (event.type) {
+                        case 'restart': {
+                            void checkAndRun(async () => {
+                                await this.manager.discord.relayRconMessage('Scheduled Restart!');
+                                await this.manager.monitor.killServer();
+                            });
+                            break;
                         }
-                    },
-                ),
+                        case 'message': {
+                            void checkAndRun(() => this.manager.rcon.global(event.params[0]));
+                            break;
+                        }
+                        case 'kickAll': {
+                            void checkAndRun(() => void this.manager.rcon.kickAll());
+                            break;
+                        }
+                        case 'lock': {
+                            void checkAndRun(() => void this.manager.rcon.lock());
+                            break;
+                        }
+                        case 'unlock': {
+                            void checkAndRun(() => void this.manager.rcon.unlock());
+                            break;
+                        }
+                        case 'backup': {
+                            void runTask(() => this.manager.backup.createBackup());
+                            break;
+                        }
+                        default: {
+                            break;
+                        }
+                    }
+                },
             );
+
+            this.log.log(
+                LogLevel.INFO,
+                `Scheduled '${event.name}' with pattern: ${event.cron} (Next run: ${job.nextInvocation().toISOString()})`,
+            );
+
+            this.tasks.push(job);
         }
     }
 
@@ -73,7 +89,7 @@ export class Events implements IStatefulService {
             try {
                 task.cancel();
             } catch (e) {
-                this.log.log(LogLevel.DEBUG, `Stopping event schedule for ${task.name} failed`, e);
+                this.log.log(LogLevel.DEBUG, `Stopping event schedule for '${task.name}' failed`, e);
             }
         }
         this.tasks = [];
